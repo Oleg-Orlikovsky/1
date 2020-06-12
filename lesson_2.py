2e646dbb1c9013a5bae87213eb9684114f54f56a
Oleg-Orlikovsky <omkaraprada@mail.ru>
2020-06-12 23:27:46 +0300

    it's my 2nd lesson on python

--- lesson_2.py
+++ lesson_2.py
@@ -1,4 +1,4 @@
-name = input("Enter seconds - ")
-a = int(input)
-b = int(input)
-c = int(input)
+seconds = int(input("Введите время в секундах:"))
+minutes = seconds // 60
+hours = minutes // 60
+print("Время %02d:%02d:%02d" % (hours, minutes % 60, seconds % 60))
